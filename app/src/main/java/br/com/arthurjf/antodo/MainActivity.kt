b2db973e9756689f5cb2b56b3bf1b9935afb83a7
arthurjf <arthur.310100@hotmail.com>
2023-02-18 08:25:38 -0300

    Projeto reestruturado

--- app/src/main/java/br/com/arthurjf/antodo/MainActivity.kt
+++ app/src/main/java/br/com/arthurjf/antodo/MainActivity.kt
@@ -2,10 +2,17 @@ package br.com.arthurjf.antodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.arthurjf.antodo.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/br/com/arthurjf/antodo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.arthurjf.antodo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
